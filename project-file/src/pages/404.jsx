64b563b2866bde6d5c1dbb93cb28f55052aa6794
Neda Vaheb <n.vaheb.nv@gmail.com>
2024-10-27 07:55:31 -0700

    page 404 complete

--- project-file/src/pages/404.jsx
+++ project-file/src/pages/404.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import { FaArrowLeft } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 function PageNotFound() {
   return (
-    <div>
-      Page Not found
-      <Link to="/register">ثبت نام</Link>
+    <div style={{
+ display:"flex",
+ justifyContent:"center",
+ alignItems:"center",gap:"40px"
+    }}>
+    
+<button  style={{
+    border: "none",
+    background: "#f64749",
+    height:" 40px",
+    width:" 80px",
+    color: "white",
+    borderRadius: "16px",    
+    }}>
+<Link to="/register">
+
+ثبت نام
+
+  
+  </Link>
+</button>
+<img src='/404.webp' style={{width:"500px"}}/> 
     </div>
   )
 }
